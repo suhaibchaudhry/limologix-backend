3f6c9b0fdd5b353bb380516e065a1c8ac00f659a
avinash thummurugoti <avinash.thummurugoti@softwaysolutions.com>
2016-06-30 17:40:25 +0530

    Solved bug faced while adding features

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -8,5 +8,13 @@ class Vehicle < ActiveRecord::Base
   validates :license_plate_number, :hll_number, :color, presence: true
   validates :hll_number, :license_plate_number, uniqueness: true
 
+  before_update :remove_existing_features
+
   accepts_nested_attributes_for :features
+
+  private
+
+  def remove_existing_features
+    self.features.each{ |feature| feature.destroy unless feature.new_record? }
+  end
 end
