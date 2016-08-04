d4cd3dbe29268f8995701f6c6f4b1ccd2c56d6be
avinash thummurugoti <avinash.thummurugoti@softwaysolutions.com>
2016-08-04 11:32:20 +0530

    Modified driver array serializer

--- app/serializers/drivers_array_serializer.rb
+++ app/serializers/drivers_array_serializer.rb
@@ -1,3 +1,7 @@
 class DriversArraySerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :mobile_number, :company, :email, :status
+  attributes :id, :first_name, :last_name, :mobile_number, :company, :email, :status, :vehicle_type
+
+  def vehicle_type
+    object.vehicle.vehicle_type.name
+  end
 end
