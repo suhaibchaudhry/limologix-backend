75b3618a0cb6035c7c2030f485d59f0404007258
avinash thummurugoti <avinash.thummurugoti@softwaysolutions.com>
2016-08-10 14:56:41 +0530

    Modified driver serializers

--- app/serializers/driver_vehicle_serializer.rb
+++ app/serializers/driver_vehicle_serializer.rb
@@ -1,7 +1,7 @@
 class DriverVehicleSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :mobile_number, :email, :company, :license_number, :license_expiry_date,
   :license_image, :badge_number, :badge_expiry_date, :ara_image, :ara_expiry_date, :insurance_company,
-  :insurance_policy_number, :insurance_expiry_date, :channel, :status
+  :insurance_policy_number, :insurance_expiry_date, :channel, :toll_credit, :status
 
   has_one :address
   has_one :vehicle
