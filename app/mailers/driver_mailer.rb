28c98ddae6048d58e051d01405543f7a94135b16
avinash thummurugoti <avinash.thummurugoti@softwaysolutions.com>
2016-09-09 13:24:56 +0530

    type added to driver_mailer

--- app/mailers/driver_mailer.rb
+++ app/mailers/driver_mailer.rb
@@ -2,7 +2,8 @@ class DriverMailer < ApplicationMailer
 
   def reset_password_mail(driver)
     @driver = driver
-    @reset_link  = "http://limologix.softwaystaging.com/#/core/reset_password?token=#{@driver.reset_password_token}"
+    type = driver.class.name
+    @reset_link  = "http://limologix.softwaystaging.com/#/core/reset_password?token=#{@driver.reset_password_token}&type=#{type}"
     mail(to: @driver.email, subject: 'Reset password instructions')
   end
 end
