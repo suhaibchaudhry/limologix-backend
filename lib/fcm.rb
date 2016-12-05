91557b7cf56742604a877c11dde94557df65c637
vivek.tripathi <vivek.tripathi@softwaysolutions.com>
2016-12-05 14:12:18 +0530

    fcm secrets distinguished for android and ios

--- lib/fcm.rb
+++ lib/fcm.rb
@@ -1,8 +1,4 @@
 class Fcm
-  HEADERS = {
-    "Authorization" => "key=#{Rails.application.secrets.fcm_authorization_key}",
-    "Content-Type" => "application/json"
-  }
   URL = 'https://fcm.googleapis.com/fcm/send'
 
   def self.publish_to_topic(topic, title, body, data )
@@ -18,7 +14,10 @@ class Fcm
           click_action: "FCM_PLUGIN_ACTIVITY"
         }
       }
-      response = do_post_request(args)
+
+      driver_record = $redis.hgetall("drivers")[topic]
+      device_type = JSON.parse(driver_record)['platform'].downcase
+      response = do_post_request(args, device_type)
 
       if response.code == "200"
         { status: "success", message: response.body }
@@ -32,13 +31,18 @@ class Fcm
 
   private
 
-  def self.do_post_request(args)
+  def self.do_post_request(args, device_type)
     uri = URI.parse(URL)
+
+    headers = {
+      "Authorization" => "key=#{Rails.application.secrets.fcm_authorization_key[device_type]}",
+      "Content-Type" => "application/json"
+    }
 
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
 
-    req = Net::HTTP::Post.new(uri.request_uri, initheader = HEADERS)
+    req = Net::HTTP::Post.new(uri.request_uri, initheader = headers)
     req.body = args.to_json
     result = http.request(req)
   end
