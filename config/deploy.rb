30b2f8fde2681f4a940803080785ca078c9c0748
vivek.tripathi <vivek.tripathi@softwaysolutions.com>
2016-12-07 14:24:15 +0530

    sidekiq restart task added to deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,3 +40,30 @@ namespace :deploy do
 
   after :migrate, :seed
 end
+
+namespace :sidekiq do
+
+  task :restart do
+    invoke 'sidekiq:stop'
+    invoke 'sidekiq:start'
+  end
+
+  before 'deploy:finished', 'sidekiq:restart'
+
+  task :stop do
+    on roles(:app) do
+      within current_path do
+        pid = p capture "ps aux | grep sidekiq | awk '{print $2}' | sed -n 1p"
+        execute("kill -9 #{pid}")
+      end
+    end
+  end
+
+  task :start do
+    on roles(:app) do
+      within current_path do
+        execute :bundle, "exec sidekiq -e #{fetch(:stage)} -C config/sidekiq.yml -d"
+      end
+    end
+  end
+end
