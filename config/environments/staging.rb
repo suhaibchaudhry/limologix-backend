69dea170b6b74f160fc04201d218bd7a0107a6de
Asad Hasan <asadpakistani@gmail.com>
2017-03-05 20:37:38 -0600

    Fix SMTP

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -79,15 +79,15 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-  config.action_mailer.default_url_options = { host: 'http://limologix.softwaystaging.com' }
+  config.action_mailer.default_url_options = { host: 'https://dispatch.limologix.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    user_name: 'softway.g',
-    password: '$0ftway123',
+    user_name: 'support@limologix.com',
+    password: 'Limologix2017!1',
     authentication: 'plain',
     enable_starttls_auto: true
   }
